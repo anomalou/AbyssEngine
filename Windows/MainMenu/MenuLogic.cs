cc6a489af646659b5f69cf29122efbc5c34e2a9f
Alexandr <31178187+calcq@users.noreply.github.com>
2020-08-06 14:14:29 +0300

    Adapted for AbyssLibraries

--- Windows/MainMenu/MenuLogic.cs
+++ Windows/MainMenu/MenuLogic.cs
@@ -3,22 +3,16 @@ using AbyssLibraries;
 namespace AbyssBehavior{
     class MenuLogic:Logic{
 
-        protected action test1, exit, start;
+        protected action exit, start;
         public MenuLogic(Window parent):base(parent){
 
         }
 
         public override void Initialization(){
-            test1 = OpenTest;
             exit = Exit;
             start = Start;
-            menu.Add("test1", test1);
             menu.Add("exit", exit);
             menu.Add("start", start);
-        }
-
-        protected void OpenTest(){
-            Core.OpenWindow(new TestWindow(), parent);
         }
 
         protected void Start(){
